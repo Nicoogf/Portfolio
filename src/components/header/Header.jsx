8ce2174b7304372d9fb940227bb50dac5e6af13a
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-15 00:12:51 -0300

    Se agregan componentes del Download y Conctact

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react' ;
 import "./header.css" ;
+import CTA from "./CTA" ;
+import ME from "../../assets/mee.jpg" ;
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+
+      <div className="container header_container">
+          <h5> Hola ,Yo soy</h5>
+          <h1> Nicolas Falabella</h1>
+          <h5 className='text-light'> FullStack Developer </h5>
+          <CTA /> 
+
+          <div className="mr">
+
+            <img src={ME} alt="me"/>
+
+          </div>
+
+          <a href="#conctact" className="scroll_down"> Ir abajo</a>
+      </div>
+
+    </header> 
   )
 }
 
---
+++ src/components/header/HeadersSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeadersSocials = () => {
+  return (
+   <div className="header__socials">
+
+    <a href='https://www.linkedin.com' target='_blank'></a>
+
+    <a href='https://github.com/' target='_blank'></a>
+
+    <a href='https://www.instagram.com' target='_blank'></a>
+
+   </div>
+  )
+}
+
+export default HeadersSocials ;
