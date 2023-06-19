ec596405a5be9c59e656ec97791c349b78a78adc
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-18 22:16:22 -0300

    Se termina el header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <div className='cta'>
 
         <a href={cv} download className='btn'> Download CV</a>
-        <a href='#contact' className='btn btn-primaryde'> Conctactame </a>
+        <a href='#contact' className='btn btn-primary'> Conctactame </a>
 
     </div>
   )
