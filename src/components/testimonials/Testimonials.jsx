953a59465eab10d4a720385296525a4f9b32a4d7
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-19 04:14:56 -0300

    Se agregan certificados

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,59 @@
 import React from 'react' ;
 import "./testimonials.css" ;
+import CERTIFICADO1 from "../../assets/titulo1.jpg" ;
 
 const Testimonials = () => {
   return (
-    <section id="testimonials"> Testimonios </section>
+    <section id="testimonials"> 
+    
+      <h5>Certificados </h5>
+      <h2>Oficiales</h2>
+
+      <div className="container testimonials__container">
+
+
+      <div className="container testimonials_container">
+        <article className='testimonial'>    
+            <div className="client__avatar">
+                  <img src={CERTIFICADO1} alt="Primer Certificado" />
+            </div>
+            <h5 className='client_name'>Full Stack Developer</h5>
+          <small className='client_review'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
+          </small>
+        </article>
+      </div>
+      
+      <div className="container testimonials_container">
+        <article className='testimonial'>    
+            <div className="client__avatar">
+                  <img src={CERTIFICADO1} alt="Primer Certificado" />
+            </div>
+            <h5 className='client_name'>Full Stack Developer</h5>
+          <small className='client_review'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
+          </small>
+        </article>
+      </div>
+      
+
+      <div className="container testimonials_container">
+        <article className='testimonial'>    
+            <div className="client__avatar">
+                  <img src={CERTIFICADO1} alt="Primer Certificado" />
+            </div>
+            <h5 className='client_name'>Full Stack Developer</h5>
+          <small className='client_review'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
+          </small>
+        </article>
+      </div>
+      
+      
+        
+      </div>
+
+    </section>
   )
 }
 
