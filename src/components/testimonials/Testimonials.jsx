5700012fee15e452be31e8620f9c3b7b7d09ebba
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-19 22:33:36 -0300

    Se agrega el swiper a la galeria de certificado

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,17 @@ import React from 'react' ;
 import "./testimonials.css" ;
 import CERTIFICADO1 from "../../assets/titulo1.jpg" ;
 
+import {  Pagination  } from 'swiper' ;
+import { Swiper, SwiperSlide } from 'swiper/react' ;
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+
 const Testimonials = () => {
   return (
     <section id="testimonials"> 
@@ -11,34 +22,18 @@ const Testimonials = () => {
 
       <div className="container testimonials__container">
 
+        
 
-      <div className="container testimonials_container">
-        <article className='testimonial'>    
-            <div className="client__avatar">
-                  <img src={CERTIFICADO1} alt="Primer Certificado" />
-            </div>
-            <h5 className='client_name'>Full Stack Developer</h5>
-          <small className='client_review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
-          </small>
-        </article>
-      </div>
-      
-      <div className="container testimonials_container">
-        <article className='testimonial'>    
-            <div className="client__avatar">
-                  <img src={CERTIFICADO1} alt="Primer Certificado" />
-            </div>
-            <h5 className='client_name'>Full Stack Developer</h5>
-          <small className='client_review'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
-          </small>
-        </article>
-      </div>
-      
 
-      <div className="container testimonials_container">
-        <article className='testimonial'>    
+      <Swiper className="container testimonials_container"
+       modules={[ Pagination ]}
+       spaceBetween={40}
+       slidesPerView={1}
+       
+       pagination={{ clickable: true }}     
+      >
+
+        <SwiperSlide className='testimonial'>    
             <div className="client__avatar">
                   <img src={CERTIFICADO1} alt="Primer Certificado" />
             </div>
@@ -46,8 +41,29 @@ const Testimonials = () => {
           <small className='client_review'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
           </small>
-        </article>
-      </div>
+        </SwiperSlide>
+
+        <SwiperSlide className='testimonial'>    
+            <div className="client__avatar">
+                  <img src={CERTIFICADO1} alt="Primer Certificado" />
+            </div>
+            <h5 className='client_name'>Full Stack Developer</h5>
+          <small className='client_review'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
+          </small>
+        </SwiperSlide>
+
+        <SwiperSlide className='testimonial'>    
+            <div className="client__avatar">
+                  <img src={CERTIFICADO1} alt="Primer Certificado" />
+            </div>
+            <h5 className='client_name'>Full Stack Developer</h5>
+          <small className='client_review'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo unde, laborum dignissimos voluptas asperiores blanditiis repudiandae nesciunt voluptatem officiis, necessitatibus laboriosam illum, aspernatur animi expedita eum. Voluptatibus, aspernatur sapiente.
+          </small>
+        </SwiperSlide>
+
+      </Swiper>
       
       
         
