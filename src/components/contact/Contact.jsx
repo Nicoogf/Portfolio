a0fab2fee088f7e1d92a5ad6888a78c4d12198d4
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-20 04:15:15 -0300

    Se termina la seccion contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,60 @@
 import React from 'react' ;
 import "./contact.css" ;
+import {MdOutlineEmail} from "react-icons/md" ;
+import {AiOutlineInstagram} from "react-icons/ai" ;
+import {BsWhatsapp} from "react-icons/bs" ;
+
 
 const Contact = () => {
   return (
-    <section id="contact"> Contact </section>
+    <section id="contact"> 
+    
+    <h5>Para contactarse</h5>
+    <h2>Mandar Email</h2>
+
+    <div className="container contact__container">
+
+      <div className="contact__options">
+
+       <article className='contact__option'>
+
+         <MdOutlineEmail className='contact__options-icon'/>
+           <h4> Email </h4>
+           <h5> Nicolasgfalabella@gmail.com </h5>
+           <a href="mailto:Nicolasgfalabella@gmail.com" target='_blank'> Enviar Mensaje </a>
+
+       </article>
+
+       <article className='contact__option'>
+        
+        <BsWhatsapp className='contact__options-icon'/>
+          <h4> Whatsapp </h4>
+          <h5> Nicolas Falabella </h5>
+          <a href="https://api.whatsapp.com/send?phone=5492324584106/" target='_blank'> Enviar Mensaje </a>
+
+     </article>
+
+     <article className='contact__option'>
+        
+        <AiOutlineInstagram className='contact__option-icon' />
+          <h4> Instagram </h4>
+          <h5> nfalabella_ </h5>
+          <a href="https://www.instagram.com/nfalabella_/" target='_blank'> Enviar Mensaje </a>
+
+     </article>
+
+      </div>
+
+      <form action="">
+        <input type="text" name="name" placeholder='Ingrese su nombre completo' required />
+        <input type="email" name="email" placeholder='Ingresar Email' required/>
+        <textarea name="message" rows="7" placeholder='Ingresar Mensaje' required></textarea>
+        <button type='submit' className='btn btn-primary'>Enviar Mensaje</button>
+      </form>
+    </div>
+
+
+   </section>
   )
 }
 
