31c4ea7a0cfd405649b49530fc4a6448c676e38d
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-20 10:51:23 -0300

    Diseño del Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import "./footer.css" ;
+import {FaFacebookF} from "react-icons/fa" ;
+import {FiInstagram} from "react-icons/fi" ;
+import {AiOutlineLinkedin} from "react-icons/ai"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <a href="#" className='footer_logo'>NGF</a>
+
+      <ul className='permalinks'>
+
+      <li><a href="">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="Services">Servicios</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#testimonials">Certificados</a></li>
+      <li><a href="#contact">Contacto</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="hppts://facebook.com"> <FaFacebookF/> </a>
+        <a href="hppts://facebook.com"> <FiInstagram/> </a>
+        <a href="https://facebook.com"> <AiOutlineLinkedin/> </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Portfolio realizao con ReactJS 2023. Todos los derechos reservados </small>
+      </div>
+
+    </footer>
   )
 }
 
